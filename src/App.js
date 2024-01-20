b7d12683f28582e3185e42054ce873c73f124b07
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-01-20 12:42:50 +0000

    It works! Drag and drop working individually

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import logo from './images/christmas-tree-vector-transparent-bg.png';
+import React from 'react';
+import Draggable from 'react-draggable';
+
+
+import tree from './images/christmas-tree-vector-transparent-bg.png';
 import blue from './images/decoration_blue.png';
 import green from './images/decoration_green.png';
 import orange from './images/decoration_orange.png';
@@ -10,22 +14,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      <header className="header">
-        <img src={logo} className="xmastree" alt="Xmas Tree" />
-        <p>
-          Decorate the Xmas Tree!
-        </p>
-        <div id="decorationsbox">
-        <img src={blue} id = "blue" className="decorations" alt = "Xmas Decoration" />
-        <img src={green} id = "green" className="decorations" alt = "Xmas Decoration" />
-        <img src={orange} id = "orange" className="decorations" alt = "Xmas Decoration" />
-        <img src={purple} id = "purple" className="decorations" alt = "Xmas Decoration" />
-        <img src={red} id = "red" className="decorations" alt = "Xmas Decoration" />
-        <img src={turquoise} id = "turquoise" className="decorations" alt = "Xmas Decoration" />
-        </div>
-      </header>
+    
+      <div>
+    <img src={tree} className="xmastree" alt="Xmas Tree"/>
+    <Draggable className="bluedrag">
+    <div><img src={blue} className="decorations" alt="Xmas Decoration"/></div>
+    </Draggable>
+    <Draggable className="bluedrag">
+    <div><img src={orange} className="decorations" alt="Xmas Decoration"/></div>
+    </Draggable>
+    <Draggable className="bluedrag">
+    <div><img src={purple} className="decorations" alt="Xmas Decoration"/></div>
+    </Draggable>
+    <Draggable className="bluedrag">
+    <div><img src={green} className="decorations" alt="Xmas Decoration"/></div>
+    </Draggable>
+    <Draggable className="bluedrag">
+    <div><img src={red} className="decorations" alt="Xmas Decoration"/></div>
+    </Draggable>
+    <Draggable className="bluedrag">
+    <div><img src={turquoise} className="decorations" alt="Xmas Decoration"/></div>
+    </Draggable>
     </div>
+
   );
 }
 
