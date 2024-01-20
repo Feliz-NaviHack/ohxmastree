c62354bb8b06d8fa5f4e0ebe9cf7cf9d4679164e
gipo-ski <koladiji@gmail.com>
2024-01-20 14:09:03 +0000

    added h1 heading and star decoration to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Draggable from 'react-draggable';
 
-
 import tree from './images/christmas-tree-vector-transparent-bg.png';
 import blue from './images/decoration_blue.png';
 import green from './images/decoration_green.png';
@@ -9,6 +8,7 @@ import orange from './images/decoration_orange.png';
 import purple from './images/decoration_purple.png';
 import red from './images/decoration_red.png';
 import turquoise from './images/decoration_turquoise.png';
+import star from './images/star.png';
 
 import './App.css';
 
@@ -17,9 +17,10 @@ function App() {
     
     <div className="App">
       <div className="main">
+        <h1>Decorate the Christmas Tree!</h1>
       <img src={tree} className="xmastree" alt="Xmas Tree"/>
       <div className="decorationsbox">
-      <p>this is the decorations box</p>
+      <h4>This is the decorations box</h4>
         <Draggable className="bluedrag">
           <div><img src={blue} className="decorations" alt="Xmas Decoration"/></div>
         </Draggable>
@@ -37,6 +38,9 @@ function App() {
         </Draggable>
         <Draggable className="turquoisedrag">
           <div><img src={turquoise} className="decorations" alt="Xmas Decoration"/></div>
+        </Draggable>
+        <Draggable className="stardrag">
+          <div><img src={star} className="decorations" alt="Xmas Decoration"/></div>
         </Draggable>
     </div>
     </div>
